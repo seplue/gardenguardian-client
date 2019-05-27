3389619402ba58be4882ecf765bdc855311dc32c
seplue <32180184+seplue@users.noreply.github.com>
2019-05-27 04:03:29 +0200

    moved routing folders. changed App.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router} from "react-router-dom";
 
-function App() {
-  return (
-<Router>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-</Router>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <Header height={"100"} />
+          <AppRouter />
+        </div>
+    );
+  }
 }
 
 export default App;
