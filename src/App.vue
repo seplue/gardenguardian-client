c4eeec44fc65199e8f3d9f32812250603de14cf2
seplue <severin.pluess@uzh.ch>
2019-08-24 10:12:03 +0200

    test_latest works in browser

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TestLatest></TestLatest>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TestLatest from "@/components/TestLatest";
 
 export default {
   name: 'app',
   components: {
+    TestLatest,
     HelloWorld
   }
 }
---
+++ src/components/TestLatest.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>GardenGuardian</h1>
+        <h2>{{info[0].garden_name}}</h2>
+        <h3>{{info[0].bed_name}}</h3>
+        <h4>{{info[0].measurement_time}}</h4>
+
+        <div
+            v-for="garden in info"
+            class="garden"
+            >
+            {{garden.measurement_type}}: {{garden.measurement_value}}
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    const axios = require("axios");
+    export default {
+        name: "TestLatest",
+        data() {
+            return {
+                info: null,
+            }
+        },
+        mounted() {
+            axios
+                .get("http://kornspeicher94.internet-box.ch:31500/test_latest")
+                .then(response => {
+                    this.info = response.data
+                })
+        }
+
+    }
+</script>
+
+
+<style scoped>
+    div {
+        color:##ad399c;
+        border: 3px solid #6aa719;
+        background: #bcda95;
+    }
+
+</style>
+
+
